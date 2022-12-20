1a7b89dda43209afe82007d78d8bee0dd7134d10
hornstrandir <paul_busch@gmx.de>
2022-12-20 16:11:10 +0100

    Calculate max number of activities for instances.

--- utils/preprocess_data.py
+++ utils/preprocess_data.py
@@ -8,11 +8,14 @@ IN_FILES = [file for file in os.listdir(DIR_INFILE) if os.path.isfile(DIR_INFILE
 def get_max_alternatives(splitted_line):
     max_number_operation = 0
     idx = 1
+    number_activities = 0
     while idx < len(splitted_line):
+        number_activities += 1
         number_operations = int(splitted_line[idx])
         max_number_operation = max(max_number_operation, number_operations)
         idx += 1 + 2 * number_operations
-    return max_number_operation
+    return max_number_operation, number_activities
+
 
 
 if __name__ == "__main__":
@@ -20,20 +23,23 @@ if __name__ == "__main__":
         infile_path = DIR_INFILE / file
         outfile_path = DIR_OUTFILE / file
         max_number_operation = 0
+        number_activities = 0
 
         with open(infile_path, 'r') as infile:
             for line_count, line in enumerate(infile):
                 if line_count == 0:
                     continue
                 splitted_line = line.split()
-                max_number_operation = max(max_number_operation, get_max_alternatives(splitted_line))
-        
+                number_activities = max(number_activities, get_max_alternatives(splitted_line)[1])
+                max_number_operation = max(max_number_operation, get_max_alternatives(splitted_line)[0])
+
         with open(infile_path, 'r') as infile, open(outfile_path, 'w') as outfile:
             for line_count, line in enumerate(infile):
                 print(f"line: {line}")
                 if line_count == 0:
                     splitted_line = line.split()
-                    splitted_line[-1] = max_number_operation
+                    splitted_line[-1] = number_activities
+                    splitted_line.append(max_number_operation)
                     outfile.write("  ".join(map(str,splitted_line)) + "\n")
                 else:
                     outfile.write(line)
